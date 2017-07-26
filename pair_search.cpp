4ad72293218a1e0074e7dc5e80b1cefa5910310f
B-Cornell <bhcornell@ucdavis.edu>
2017-07-25 20:49:25 -0700

    Add 5Mpc compatibility and cleaned up math

--- pair_search.cpp
+++ pair_search.cpp
@@ -24,7 +24,7 @@ int main(){
   double angle_prob;
   double total_prob;
   int rel_vel_angle;
-  double z_vel, y_vel;
+  double v_y, v_z, y_vel, z_vel, obsvel, obssep;
   int returning[2];
   double rel_vel[91];
   
@@ -33,18 +33,9 @@ int main(){
 
   int i,j,k,l,m, pair_count=0;
 
-  string save_directory = "/home/Projects/Darksky/Catalog/DSS_Scripts/";
+  string save_directory = "/home/DSS_Scripts/";
 
-/*how many analogs
 
- * sphere[theta][phi]
- * Theta has a range of 0 to pi.
- * Phi has a range of 0 to 2pi.
- * Since phi covers twice the interval, we do ANGULAR_RES*2.
- * This array stores all the viewing angles that fulfil the search criterion.
- * Printing this array is the same as taking the surface of a sphere and flattening and stretching it into a squre.
- * Angular resolution determines the number of "pixels" on this sphere.
- */
   char sphere[ANGULAR_RES][ANGULAR_RES*2];
 
   b_sep = get_range_input("separation"); // Units: Mpc
@@ -55,7 +46,7 @@ int main(){
   cout << "------------------------------------------" << endl;
 
   ifstream f_halo_data;
-  f_halo_data.open("reduced_halo_pairs_full_data_3500Kpc.txt");
+  f_halo_data.open("reduced_cosmo_pairs.txt");
 
   if (f_halo_data.is_open()){
 
@@ -68,8 +59,8 @@ int main(){
     for(i=0; i< N_PAIRS; i++){
 
       getline(f_halo_data,pair_id_str); //halo pair id
-      getline(f_halo_data,halo_a_str); // halo a data
-      getline(f_halo_data,halo_b_str); // halo b data
+      getline(f_halo_data,halo_a_str);  // halo a data
+      getline(f_halo_data,halo_b_str);  // halo b data
 
       pair[i].id = atoi(pair_id_str.c_str());
       pair[i].a = halo_t_parser(halo_a_str); // Parses pair.a data into halo_t retainer
@@ -90,14 +81,14 @@ int main(){
   cout << "Searching for matching pairs." << endl;
 
   
-  std::ofstream pyfile;
-  pyfile.open("pyfile.csv");
-  pyfile << "#HaloID, Mass 1, Mass 2, Separation, Velocity on axis, Velocity off axis, Returning, Relative Velocity Angle, Angle probs, Total prob";
+  std::ofstream savefile;
+  savefile.open("savefile.csv");
+  
   
 
   // Iterates over all the pairs
   for(k=0; k < N_PAIRS; k++){
-
+    
     // Print progress in percentage
     if (k%1000 == 0){
       cout <<  double(k)/double(N_PAIRS)*100 << '%' << endl;
@@ -114,20 +105,21 @@ int main(){
         ( ((pair[k].a.mvir > b_mass_b.low) && (pair[k].a.mvir <  b_mass_b.up))    &&
           ((pair[k].b.mvir > b_mass_a.low) && (pair[k].b.mvir <  b_mass_a.up)) )  ){
     pair_count++;
+    
+    //function rotates pair to make calculations easier
     calculation_pair = temp_halo(pair[k]);
       
-      
-    pyfile << pair[k].id << ", " << calculation_pair.a.mvir << ", " << calculation_pair.b.mvir << ", " << calculation_pair.b.pos.z << ", " << calculation_pair.b.vel.z << ", " << calculation_pair.b.vel.y << ", ";   
-      /*find the velocity directions*/
+
+    //find the velocity directions to see if it is inbound or outbound
     if (calculation_pair.b.vel.z < 0.0){
       z_vel = -calculation_pair.b.vel.z;
       returning[0]++;
-      pyfile << "1, ";
+     
     }
     else {
       z_vel = calculation_pair.b.vel.z;
       returning[1]++;
-      pyfile << "0, ";
+  
       }
     if (calculation_pair.b.vel.y < 0.0){
       y_vel = -calculation_pair.b.vel.y;
@@ -137,13 +129,20 @@ int main(){
 
     }
     rel_vel_angle = atan(y_vel/z_vel) * (180 / PI);
-    rel_vel[rel_vel_angle]++;
+
       
-    pyfile << rel_vel_angle<< ", " ;
+    
+    //add in the if statement to filter out all inbound pairs
+    //if (calculation_pair.b.vel.z > 0){  
+    
+    //writes the pair data to the save file
+      savefile << pair[k].id << ", " << pair[k].a.index << ", " << pair[k].b.index << ", " << calculation_pair.a.mvir << ", " << calculation_pair.b.mvir << ", " << calculation_pair.b.pos.z << ", " << calculation_pair.b.vel.z << ", " << calculation_pair.b.vel.y << ", "  << "0, " << rel_vel_angle<< ", ";
       
-      
+    //starts iterating through the possible viewing angles
     for(i = 0; sph.theta <= double(PI); i++){
       sph.theta = sph.theta + (double(PI)/double(ANGULAR_RES)); // Range for theta is 0 to pi. 
+      
+      //divisor is the number of points that should go around each viewing angle. This way there are a near even distribution of points we view from 
       divisor = (ANGULAR_RES) * sin(sph.theta);
       total_divisor_points += divisor;
       points = int(divisor);
@@ -151,73 +150,70 @@ int main(){
       sph.phi = 0.0;
     
       for( j = 0; sph.phi < double(2*PI) and points>0; j++){
-        sph.phi = sph.phi + (double(PI*2.0))/points; // Range for phi is 0 to 2pi
+        sph.phi = sph.phi + (double(PI*2.0))/points; // Range for phi is 0 to 2pi and the number of points are detirmined above
    
-        obs = sph_to_cart(sph); // Convert spherical coordinates to cartesian
-	
-        rel_p = get_rel_p(calculation_pair.a,calculation_pair.b); // Calculate relative position
-        rel_v = get_rel_v(calculation_pair.a,calculation_pair.b); // Calculate relative velocity
-
-        obs_vel = projection(rel_v,obs); // Calculate observed velocity
-        obs_sep = sep_projection(rel_p,obs); // Calculate observed separation
-
-        mass_a_a_prob = probability("gaussian", b_mass_a.up - ((b_mass_a.up - b_mass_a.low)/2.), ((b_mass_a.up - b_mass_a.low)/2.), calculation_pair.a.mvir);//probability that the mass of the first halo in the pair matches the first halo in the musketball
-
-	mass_a_b_prob = probability("gaussian", b_mass_a.up - ((b_mass_a.up - b_mass_a.low)/2.), ((b_mass_a.up - b_mass_a.low)/2.), calculation_pair.b.mvir);//probability that the mass of the first halo in the pair matches the second halo in the musketball
-	
-	mass_b_a_prob = probability("gaussian", b_mass_b.up - ((b_mass_b.up - b_mass_b.low)/2.), ((b_mass_b.up - b_mass_b.low)/2.), calculation_pair.a.mvir);//probability that the mass of the second halo in the pair matches the first halo in the musketball
-
-	mass_b_b_prob = probability("gaussian", b_mass_b.up - ((b_mass_b.up - b_mass_b.low)/2.), ((b_mass_b.up - b_mass_b.low)/2.), calculation_pair.b.mvir);//probability that the mass of the second halo in the pair matches the second halo in the musketball
-
-        vel_prob = probability("gaussian", b_vel.up - ((b_vel.up - b_vel.low)/2.), ((b_vel.up - b_vel.low)/2.), magnitude(obs_vel));
-
-	sep_prob = probability("gaussian", b_sep.up - ((b_sep.up - b_sep.low)/2.), ((b_sep.up - b_sep.low)/2.), magnitude(obs_sep));
-	
-	total_prob = (mass_a_a_prob*mass_b_b_prob+mass_a_b_prob*mass_b_a_prob)*vel_prob*sep_prob;
-
         
-        area_counter += double(total_prob)*(double(divisor)/(double(points)*double(points)));
-	if (azimuthal[i] == azimuthal[i]){
-	  if (i <50){
-	    azimuthal[i] += double(total_prob)*(divisor/(double(points)*double(points)));
-	    
-	    azitemp[i] += double(total_prob)*(divisor/(double(points)*double(points)));
-	  }
-	  else {
-	    azimuthal[99-i] += double(total_prob)*(divisor/(double(points)*double(points)));
-	    azitemp[99-i] += double(total_prob)*(divisor/(double(points)*double(points)));
-	  }
-	}
+        //The math for this section is made easier by the rotation done by the temphalo() function
+        obs.x = sin(sph.theta)*cos(sph.phi);
+        obs.y = sin(sph.theta)*sin(sph.phi);
+        obs.z = cos(sph.theta);
+        v_y = calculation_pair.b.vel.y;
+        v_z = calculation_pair.b.vel.z;
+
+        obsvel = obs.y*v_y+obs.z*v_z;// Calculate observed velocity
+        if(obsvel < 0){obsvel = -obsvel;} //Should always be positive
+        
+        obssep = calculation_pair.b.pos.z*sin(sph.theta); // Calculate observed separation
+        if(obssep < 0){obssep = -obssep;}
+
+        //Calculate all of the probabilities
+
+        mass_a_a_prob = probability("gaussian", b_mass_a.up - ((b_mass_a.up - b_mass_a.low)/2.), ((b_mass_a.up - b_mass_a.low)/4.), calculation_pair.a.mvir);//probability that the mass of the first halo in the pair matches the first halo in the musketball
+
+	    mass_a_b_prob = probability("gaussian", b_mass_a.up - ((b_mass_a.up - b_mass_a.low)/2.), ((b_mass_a.up - b_mass_a.low)/4.), calculation_pair.b.mvir);//probability that the mass of the first halo in the pair matches the second halo in the musketball
 	
+	    mass_b_a_prob = probability("gaussian", b_mass_b.up - ((b_mass_b.up - b_mass_b.low)/2.), ((b_mass_b.up - b_mass_b.low)/4.), calculation_pair.a.mvir);//probability that the mass of the second halo in the pair matches the first halo in the musketball
+
+	    mass_b_b_prob = probability("gaussian", b_mass_b.up - ((b_mass_b.up - b_mass_b.low)/2.), ((b_mass_b.up - b_mass_b.low)/4.), calculation_pair.b.mvir);//probability that the mass of the second halo in the pair matches the second halo in the musketball
+
+        vel_prob = probability("gaussian", b_vel.up - ((b_vel.up - b_vel.low)/2.), ((b_vel.up - b_vel.low)/4.), obsvel);
+
+	    sep_prob = probability("gaussian", b_sep.up - ((b_sep.up - b_sep.low)/2.), ((b_sep.up - b_sep.low)/4.), obssep);
+	
+	    total_prob = (mass_a_a_prob*mass_b_b_prob+mass_a_b_prob*mass_b_a_prob)*vel_prob*sep_prob;
+        
+        // Tally to keep track of the probability added together from all viewing angles
+        area_counter += double(total_prob);
+	    
         }
       }
     
-    calculation_pair.prob = area_counter / total_divisor_points;
-    pair[k].prob = area_counter / total_divisor_points; //area that works divided by the surface area of the sphere
+    //now that we have gone through all viewing angles, put in the total probability
+    
+    calculation_pair.prob = area_counter;
+    pair[k].prob = area_counter; // Total probability across all viewing angles
     area_counter = 0;
-    i =0;
-    for(i = 0; i <50 ; i++){
-    pyfile << azitemp[i] << ",  ";
-    azitemp[i] = 0;
-    }
-    pyfile << calculation_pair.prob << endl;
+    savefile << calculation_pair.prob << endl;
 
           
-    //Print pair attributes
+    //Print pair attributes to the terminal screen
+    //Prints both original and rotated attributes
     cout << pair[k].id << endl;
+    print_halo(pair[k].a);
+    print_halo(pair[k].b);
     print_halo(calculation_pair.a);
     print_halo(calculation_pair.b);
     cout << "probability: " << pair[k].prob << endl;
     cout << "relative velocity angle: " << rel_vel_angle << endl;
     cout << "------------------------------------------" << endl;
        
-
+//}//This is the bracket for the outbound filter
 
 } 
 }
          
 
-  pyfile.close();
+  savefile.close();
 
   cout << "100%\nComplete."<< endl;
 
@@ -227,16 +223,5 @@ int main(){
   cout << "(id)                  pair_id" << endl;
   cout << "(halo a attributes)   aindex ax ay az avx avy avz amvir ar200b" << endl;
   cout << "(halo b attributes)   bindex bx by bz bvx bvy bvz bmvir br200b" << endl;
-  for(i = 0; i <50 ; i++){
-    sph.theta = sph.theta + (double(PI)/double(ANGULAR_RES));
-    cout << azimuthal[i] << ",  ";
-  }
-  cout << endl << endl;
-  for(i = 0; i < 91; i++){
-    cout << rel_vel[i] << ",  ";
-  
-  }
-  cout << endl << endl;
-  cout << returning[0] << ",  " << returning[1] << endl;
   return 0;
 }
